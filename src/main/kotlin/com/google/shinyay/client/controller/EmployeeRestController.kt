0c2777c28d56588aa45487e51fb4a9866e9496a2
shinyay <shinya.com@gmail.com>
2021-07-06 21:37:31 +0900

    Modify application properties name

--- src/main/kotlin/com/google/shinyay/client/controller/EmployeeRestController.kt
+++
@@ -1,7 +0,0 @@
-package com.google.shinyay.client.controller
-
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-class EmployeeRestController {
-}
---
+++ src/main/kotlin/com/google/shinyay/client/model/Employee.kt
@@ -0,0 +1,3 @@
+package com.google.shinyay.client.model
+
+data class Employee()
