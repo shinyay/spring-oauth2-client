b66301b3ba75ed14d2d62b015e633324cad5ae9b
shinyay <shinya.com@gmail.com>
2021-07-06 18:19:48 +0900

    Add HttpSecurity Configuration

--- src/main/kotlin/com/google/shinyay/client/config/SecurityConfig.kt
+++ src/main/kotlin/com/google/shinyay/client/config/SecurityConfig.kt
@@ -1,8 +1,17 @@
 package com.google.shinyay.client.config
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        http?.authorizeRequests { authz ->
+            authz.antMatchers("/")
+                .permitAll()
+                .anyRequest()
+        }
+            ?.oauth2Login()
+    }
 }
