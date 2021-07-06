7023d2149e04faaed5e4ca2a52eb6ba6307aec56
shinyay <shinya.com@gmail.com>
2021-07-06 17:23:34 +0900

    Add thymeleaf-extras-springsecurity5 which integrate between thymeleaf and spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
